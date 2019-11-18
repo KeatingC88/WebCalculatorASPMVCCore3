68193ab396e6f7193d1eb49f112d8cf4db196033
keatingc88@gmail.com <keatingc88@gmail.com>
2019-11-17 23:44:20 -0700

    Updated Calculator Events and working properly for the most part.
    1 Bug Issue: occurs when you click decimal once, then an operator, and on the 2nd decimal for the 2nd set of numbers, then the display loses it's previous keys, but comes back when you click a number AFTER the decimal.

--- WebCalculatorASPMVC/WebCalculatorASPMVC/wwwroot/js/CalculatorClientCore.js
+++ WebCalculatorASPMVC/WebCalculatorASPMVC/wwwroot/js/CalculatorClientCore.js
@@ -6,112 +6,90 @@
     KeysPressed = [];
     //Storage for Strings from Single Characters that are assembled.
     KeysRecorder = [];
-
+    //Input Example to use: 3.3+3.3-9+10
     key(value) {
-
+        //If numerical value...
         if (!isNaN(value)) {
-            //Its a number
             //console.log("It's a Number: " + value);
+            //Push numeric value.
+            this.KeysPressed.push(value);
+            //Set to output string.
+            this.DisplayString = this.KeysPressed.join("");
+            //Display output to the user.
+            this.display();
+            //If KeysRecorder has an Element (meaning it's not the first inital set of numbers before an Arithmetic Operator is Pressed on the KeyPad)
+            if (Object.keys(this.KeysRecorder).length > 0) {
+                var previousKeys = this.getLastRecordedKey();
+                this.DisplayString = previousKeys + this.KeysPressed.join("");
+                this.display();
+            }
+        }
 
-            console.log(this.KeysRecorder);
-            if (this.KeysRecorder === undefined || Object.keys(this.KeysRecorder).length === 0) {
-                console.log("Key Recorder is Empty");
-                //Save the Key to the KeysPressed Object.
-                this.KeysPressed.push(value);
-                this.DisplayString = this.assembleStringofKeys();
+        if (value === ".") {
+            //console.log("It's a Decimal: " + value);
+            var previousKeys = this.getLastKeysPressed();
+            //If there's no number present when the decimal key is pressed, then provide a zero before the decimal.
+            if (previousKeys === undefined || Object.keys(this.KeysPressed).length === 0) {
+                this.KeysPressed.push("0.");
+                this.DisplayString = this.KeysPressed;
                 this.display();
-            } else {
-                console.log("Key Recorder is Not Empty");
-                this.KeysPressed.push(value);
-                this.DisplayString = this.assembleStringofKeys();
+            }
+            
+            var string = this.KeysPressed.join("");
+            var find = string.includes(".");
+
+            if (find === false) {
+
+                //console.log("Find: " + find);
+                //Either 1 Decimal or None was Found
+                this.KeysPressed.push(".");
+                this.DisplayString = this.KeysPressed.join("");
                 this.display();
+
             }
             
         }
 
-        if (value === ".") {
-            //Its a Decimal
-            //console.log("It's a Decimal: " + value);
-            this.decimalManager();
-            this.DisplayString = this.assembleStringofKeys();
-            this.display();
-        }
-
         if (this.arithmeticOperatorDetector(value) !== false) {
-            //Its an Operator
-            console.log("It's an Operator: " + value);
-            //Get Current KeysPressed
-            var numbers = this.assembleStringofKeys();
-            //Save Current KeysPressed
-            this.KeysRecorder.push(numbers);            
-            //Save Current Operator
-            this.KeysRecorder.push(value);
-            //
-            this.KeysPressed.push(value);
-            console.log(this.KeysPressed);
-            this.DisplayString = this.assembleStringofKeys();
-            this.display();
-            //Clear the Previous KeysPressed
-            this.KeysPressed = [];            
-            //console.log("KeysRecorder: " + this.KeysRecorder);
-            //How to Display this?
-        }
-
-        if (value === "=") {
-            console.log("Calculate");
-            //Get the Assemble Last set of Keys Inputted by the User in the KeysPressed Object.
-            var lastSetOfNumbers = this.assembleStringofKeys();
-            //Add KeysPressed to the KeysRecorder Object.
-            this.KeysRecorder.push(lastSetOfNumbers);
-            console.log(this.KeysRecorder);
-            //Calculate the String...
-            //Calculate the KeysRecorder Method();
-            //Clear the KeysRecorder = [];
-            //Clear Current Keys Pressed
-            this.KeysPressed = [];
-            console.log("KeysPressed (On Calculate): " + this.KeysPressed);
             
+            var lastKeyPress = this.getLastKeysPressed();
+            //If there's no number presented after the decimal from when key is pressed, then provide a zero after the decimal.
+            if (lastKeyPress === undefined || Object.keys(this.KeysPressed).length === 0 || lastKeyPress === "0.") {                
+                this.KeysPressed.push("0");
+                this.DisplayString = this.KeysPressed;
+                this.display();
+            }
+
+            //console.log("It's an Operator: " + value);
+            //console.log("KeyRecorder: " + this.KeysRecorder);
+            this.KeysPressed.push(value);
+            this.KeysRecorder.push(this.KeysPressed.join(""));
+            this.DisplayString = this.KeysPressed.join("");
+            this.display();
+            this.KeysPressed = [];
         }
 
-    }
-
-    assembleStringofKeys() {
-        var res = this.KeysPressed.join("");
-        //console.log("Assemble String of Keys: " + res);
-        return res;        
-    }
-
-    decimalManager() {
-        
-        //if (first element in KeysPressed is a Decimal -- Apply the 0.) { }
-        var lastKeyPress = this.getLastKeysPressed();
-        //
-        if (lastKeyPress === undefined || Object.keys(this.KeysPressed).length === 0) {
-            //If Object is empty, apply this push to the Array.
-            this.KeysPressed.push("0.");
+        if (value === "=") {            
+            this.KeysRecorder.push(this.KeysPressed.join(""));
+            console.log("Calculate(raw):" + this.KeysRecorder);
+            console.log("Calculate(string):" + this.KeysRecorder.join(""));
+            this.DisplayString = this.KeysRecorder.join("");
+            this.display();
         }
-        //if (there is a decimal in any KeysPressed elements, disable the decimal button) { }
-        var string = this.assembleStringofKeys();
-        var find = string.includes(".");        
-        if (find === false) {
-            //console.log("Find: " + find);
-            //Either 1 Decimal or None was Found
-            this.KeysPressed.push(".");
-        } else {
-            //console.log("Find: " + find);
-            //Deimcal was Found -- do nothing to avoid doubling decimals in strings.
-        }
+
     }
 
     getLastKeysPressed() {
         var lastItem = this.KeysPressed[this.KeysPressed.length - 1];
-        //console.log("Last KeyPressed: " + lastItem);
+        console.log(this.KeysPressed);
+        console.log("Last KeyPressed: " + lastItem);
         return lastItem;
     }
 
     getLastRecordedKey() {
         var lastItem = this.KeysRecorder[this.KeysRecorder.length - 1];
-        //console.log("Last KeyRecorder: " + lastItem);
+        console.log(this.KeysRecorder);
+        console.log("Last KeyRecorder: " + lastItem);
         return lastItem;
     }
 
@@ -145,125 +123,6 @@
         document.getElementsByTagName("input")[0].setAttribute("placeholder", this.DisplayString);
     }
 
-    /*
-    //Example
-    //console.log(eval('2 * (2 + 2) / 0'));//
-    DisplayString = "";
-    KeysRecorder = [];
-    KeysPressed = [];   
-
-    //OnClick Event: For Calculator Keypad on the View. Inputs from User. Script Controller
-    key(value) {
-        
-        if (!isNaN(value)) {
-            this.userClickedNumber(value);
-        } else if (this.arithmeticOperatorDetector(value) !== false) {
-            this.userClickedOperator(value);
-        } else if (this.decimalDetector(value) !== false) {
-            this.userClickedDecimal(value);
-        } else {
-            //console.log("Error: Unassigned key");
-            this.DisplayString = "Error!";
-            this.stringDisplay();
-        }
-    }
-
-    userClickedDecimal(value) {
-
-        //
-        this.KeysPressed.push(value);
-        this.stringAssembler(this.KeysPressed);
-        this.DisplayString = this.KeysRecorder[this.KeysRecorder.length - 1];
-        this.stringDisplay();
-        //
-    }
-
-    userClickedNumber(value) {        
-        this.KeysPressed.push(value);
-        this.stringAssembler(this.KeysPressed);
-        this.DisplayString = this.KeysRecorder[this.KeysRecorder.length - 1];
-        console.log(this.DisplayString);
-        this.stringDisplay();
-    }
-
-    userClickedOperator(value) {
-
-        var checkLastCharacter = this.KeysRecorder[this.KeysRecorder.length - 1][this.KeysRecorder.length - 1];
-        var checkSomething = this.KeysRecorder[this.KeysRecorder.length - 1];
-        console.log("checkLastChar: " + checkLastCharacter);
-        console.log("checkSomething: " + checkSomething);//Clue
-        console.log(this.KeysRecorder);
-
-        if (this.arithmeticOperatorDetector(checkLastCharacter) !== false) { //This detects and prevents the user from entering 2 Operators at one after another. (+ - * /)
-            console.log("Operator Detection: " + checkLastCharacter);
-            
-            this.KeysPressed.pop();
-            this.KeysRecorder.pop();
-            this.KeysPressed.push(value);
-            this.stringAssembler(this.KeysPressed);
-            this.DisplayString = this.KeysRecorder[this.KeysRecorder.length - 1];
-            this.stringDisplay();
-
-        } else {
-
-            console.log("Default");
-            this.KeysPressed.push(value);
-            this.stringAssembler(this.KeysPressed);
-            this.DisplayString = this.KeysRecorder[this.KeysRecorder.length - 1];
-            this.stringDisplay();
-            
-        }        
-    }
-
-    stringAssembler(values) {
-        var result = values.join("");
-        this.KeysRecorder.push(result);
-        return result;
-    }
-
-    stringDisplay() {
-        document.getElementsByTagName("input")[0].setAttribute("placeholder", this.DisplayString);
-    }
-
-    decimalDetector(value) {
-        if (value !== ".") {
-            return false;
-        } else {
-            return true;
-        }
-    }
-
-    //Return Detected Math Operator or Return false if none are in the key-pressed-value.
-    arithmeticOperatorDetector(value) {        
-        switch (value) {
-            case "+":
-                return '+';
-                break;
-            case "-":
-                return '-';
-                break;
-            case '*':
-                return '*';
-                break;
-            case '/':
-                return '/';
-                break;
-            case '=':
-                return '=';
-                break;
-            default:
-                return false;                
-                break;
-        }
-    }
-    //Clear the Textbox
-    clearDisplay() {
-        this.KeysRecorder = [];
-        this.KeysPressed = [];
-        document.getElementsByTagName("input")[0].setAttribute("placeholder", "");
-    }
-
-*/
 }
 //Initiate/Instantiate the Class
 var calculator = new CalculatorEvents();
